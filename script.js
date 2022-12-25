a701d4c5fe7b71ad3c7911a620d79620ea83c481
Amanda Buttineau <114027684+ab1820@users.noreply.github.com>
2022-12-25 18:39:37 -0500

    done

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const mainContainer = document.querySelector(".main-container");
 const secondaryContainer = document.querySelector(".secondary-container");
 const buttonsContainer = document.querySelector(".buttons");
 const submitButton = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
 
 submitButton.addEventListener("click", () => {
   mainContainer.style.display = "none";
   secondaryContainer.style.display = "block"
 });
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML
+  })
+})
 
